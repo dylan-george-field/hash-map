16e22b0fc27d9d91dcc0bcb2fd3034fedb7fd175
Dylan George Field <dylan.george.field@gmail.com>
2022-12-04 13:42:19 +1100

    feat: implement a hash map using a linked list and default hashcode

---
+++ HashMap/HashMap.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsolePlayground
+{
+    internal class KeyValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return Key + ":" + Value;
+        }
+    }
+
+    internal class HashMap
+    {
+        private LinkedList<KeyValue>[] lists;
+        private readonly int _size = 0;
+
+        private const int DefaultSize = 10;
+
+        public HashMap() : this(DefaultSize) { }
+
+        public HashMap(int size)
+        {
+            _size = size;
+            lists = new LinkedList<KeyValue>[size];
+        }
+
+        public void Put(KeyValue pair)
+        {
+            var index = GetIndex(pair.Key);
+
+            var _ = GetLinkedList(index);
+
+            lists[index].AddFirst(pair);
+        }
+
+
+        public KeyValue Get(string key)
+        {
+            var index = GetIndex(key);
+
+            var list = GetLinkedList(index);
+
+            foreach (var item in list)
+            {
+                if (item.Key == key)
+                    return item;
+            }
+
+            return null;
+        }
+
+        public void Remove(string key)
+        {
+            var index = GetIndex(key);
+            
+            var list = GetLinkedList(index);
+            
+            foreach(var item in list)
+            {
+                if (item.Key == key)
+                    list.Remove(item);
+                    break;
+            }
+        }
+
+        public void Clear()
+        {
+            lists = new LinkedList<KeyValue>[_size];
+        }
+
+        private int GetIndex(string key)
+        {
+            var hash = key.GetHashCode();
+            return Math.Abs(hash % lists.Length);
+        }
+
+        private LinkedList<KeyValue> GetLinkedList(int index)
+        {
+            if (lists[index] == null)
+            {
+                lists[index] = new LinkedList<KeyValue>();
+                return lists[index];
+            }
+            else
+                return lists[index];
+        }
+    }
+}
---
+++ HashMap/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsolePlayground
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hashmap = new HashMap();
+
+            const string key1 = "Dylan";
+            const string key2 = "Ringo";
+
+            var pair1 = new KeyValue() { Key = key1, Value = "Field" };
+            var pair2 = new KeyValue() { Key = key2, Value = "Starr" };
+
+            Console.WriteLine("Add the following values to the hash table:");
+            Console.WriteLine(pair1);
+            Console.WriteLine(pair2);
+
+            hashmap.Put(pair1);
+            hashmap.Put(pair2);
+
+            Console.WriteLine("Let's get the items:");
+            Console.WriteLine(hashmap.Get(key1));
+            Console.WriteLine(hashmap.Get(key2));
+
+            Console.WriteLine("Remove 'Dylan'");
+            hashmap.Remove(key1);
+            Console.WriteLine("And try to get it");
+            Console.WriteLine(hashmap.Get(key1));
+
+            Console.WriteLine("Finally clear the hash table");
+            hashmap.Clear();
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+        }
+    }
+}
