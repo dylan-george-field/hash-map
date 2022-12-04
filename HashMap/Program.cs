a4edcec5ccda264eb598ed93bc283935e33e37f8
Dylan George Field <13308154+dylan-george-field@users.noreply.github.com>
2022-12-04 13:52:36 +1100

    feat: add case for duplicate keys

--- HashMap/Program.cs
+++ HashMap/Program.cs
@@ -6,24 +6,31 @@ namespace ConsolePlayground
     {
         public static void Main(string[] args)
         {
-            var hashmap = new HashMap();
+            var hashmap = new HashMap(2);
 
             const string key1 = "Dylan";
             const string key2 = "Ringo";
+            const string key3 = "Peter";
 
             var pair1 = new KeyValue() { Key = key1, Value = "Field" };
             var pair2 = new KeyValue() { Key = key2, Value = "Starr" };
+            var pair3 = new KeyValue() { Key = key3, Value = "Brock" };
 
             Console.WriteLine("Add the following values to the hash table:");
             Console.WriteLine(pair1);
             Console.WriteLine(pair2);
+            Console.WriteLine(pair3);
 
             hashmap.Put(pair1);
             hashmap.Put(pair2);
+            hashmap.Put(pair3);
+
+            hashmap.Put(pair1); // duplicate error
 
             Console.WriteLine("Let's get the items:");
             Console.WriteLine(hashmap.Get(key1));
             Console.WriteLine(hashmap.Get(key2));
+            Console.WriteLine(hashmap.Get(key3));
 
             Console.WriteLine("Remove 'Dylan'");
             hashmap.Remove(key1);
